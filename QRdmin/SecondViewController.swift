56dde006da73b63762331aa74782a0e93fba3123
Handler Reinhard <handlr@ke207r6513.fh-joanneum.at>
2015-10-20 15:35:13 +0200

    Initial Commit

---
+++ QRdmin/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  QRdmin
+//
+//  Created by Handler Reinhard on 20/10/15.
+//  Copyright © 2015 FH Joanneum. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
